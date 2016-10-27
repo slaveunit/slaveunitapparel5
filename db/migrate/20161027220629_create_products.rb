b6e988e98e68d4aec213ea1e55accc0cd7124e20
slave-unit <marco.abundo@live.com>
2016-10-27 22:09:38 +0000

    Rails g model for Product, OrderStatus, Order and OrderItem

---
+++ db/migrate/20161027220629_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price, precision: 12, scale: 3
+      t.boolean :active
+      t.string :size
+
+      t.timestamps null: false
+    end
+  end
+end
