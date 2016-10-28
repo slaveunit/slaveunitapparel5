b0808c4d7a3be6b1b5563f85aa9fe4d0d7845f05
slave-unit <marco.abundo@live.com>
2016-10-28 00:01:56 +0000

    Made shopping cart from https://richonrails.com/articles/building-a-shopping-cart-in-ruby-on-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
+  get 'products/index'
+  
+  
+  
+  
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+  root to: "products#index"
+  
+  
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Product.delete_all
+Product.create! id: 1, name: "Test-1", size: "Small", price: 19.50, active: true
+Product.create! id: 2, name: "Test-2", size: "Medium", price: 19.00, active: true
+Product.create! id: 3, name: "Test-3", size: "Large", price: 20.00, active: true
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
